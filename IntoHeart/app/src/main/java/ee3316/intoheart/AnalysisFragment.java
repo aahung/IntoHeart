1832cb3de12ab56446255a9d83c0e82681452adc
aahung <i@xinhong.me>
2015-04-12 23:39:19 +0800

    change disease list from activity to alertview

--- IntoHeart/app/src/main/java/ee3316/intoheart/AnalysisFragment.java
+++ IntoHeart/app/src/main/java/ee3316/intoheart/AnalysisFragment.java
@@ -1,6 +1,7 @@
 package ee3316.intoheart;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,8 +11,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
@@ -59,43 +63,70 @@ public class AnalysisFragment extends Fragment {
         btn_slow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.setClass(getActivity().getApplicationContext(),DiseaseListActivity.class);
-                intent.putExtra("kind", DiseaseListActivity.DISEASE_KIND.SLOW);
-                getActivity().startActivity(intent);
+                showSlowDisease(DiseaseListActivity.DISEASE_KIND.SLOW);
             }
         });
         Button btn_fast=(Button)rootView.findViewById(R.id.view_detail_fast_button);
         btn_fast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(getActivity().getApplicationContext(),DiseaseListActivity.class);
-                intent.putExtra("kind", DiseaseListActivity.DISEASE_KIND.FAST);
-                getActivity().startActivity(intent);
+                showSlowDisease(DiseaseListActivity.DISEASE_KIND.FAST);
             }
         });
         Button btn_slow2=(Button)rootView.findViewById(R.id.view_detail_slow_button2);
         btn_slow2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.setClass(getActivity().getApplicationContext(),DiseaseListActivity.class);
-                intent.putExtra("kind", DiseaseListActivity.DISEASE_KIND.SLOW);
-                getActivity().startActivity(intent);
+                showSlowDisease(DiseaseListActivity.DISEASE_KIND.SLOW);
             }
         });
         Button btn_fast2=(Button)rootView.findViewById(R.id.view_detail_fast_button2);
         btn_fast2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(getActivity().getApplicationContext(),DiseaseListActivity.class);
-                intent.putExtra("kind", DiseaseListActivity.DISEASE_KIND.FAST);
-                getActivity().startActivity(intent);
+                showSlowDisease(DiseaseListActivity.DISEASE_KIND.FAST);
             }
         });
         return rootView;
+    }
+
+    public void showSlowDisease(final int diseaseKind) {
+
+        final String[] diseaseNames;
+        final String[] diseaseURLs;
+        String title;
+        if (diseaseKind == DiseaseListActivity.DISEASE_KIND.SLOW) {
+            diseaseNames = getResources().getStringArray(R.array.slowdiseasename_array);
+            diseaseURLs = getResources().getStringArray(R.array.slowdiseaseurls_array);
+            title = "Slow diseases";
+        } else {
+            diseaseNames = getResources().getStringArray(R.array.fastdiseasename_array);
+            diseaseURLs = getResources().getStringArray(R.array.fastdiseaseurls_array);
+            title = "Fast diseases";
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.alertdialog_disease_list, null);
+        final ListView listView = ((ListView)dialogView.findViewById(R.id.list_view));
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, diseaseNames));
+        builder.setView(dialogView)
+                .setTitle(title);
+        final AlertDialog dialog = builder.create();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String title = diseaseNames[position];
+                String url = diseaseURLs[position];
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), DiseaseDetailActivity.class);
+                intent.putExtra("title", title);
+                intent.putExtra("url", url);
+                startActivity(intent);
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 
     @Override
